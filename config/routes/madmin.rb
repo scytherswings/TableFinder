79410e39c60bba6db825f8b940ff40983f8f7173
Andrew Newell <andrew.newell@genesys.com>
2022-01-23 16:26:58 -0500

    run more stuff

---
+++ config/routes/madmin.rb
@@ -0,0 +1,16 @@
+# Below are the routes for madmin
+namespace :madmin do
+  resources :announcements
+  namespace :active_storage do
+    resources :blobs
+  end
+  resources :users
+  resources :services
+  namespace :active_storage do
+    resources :attachments
+  end
+  namespace :active_storage do
+    resources :variant_records
+  end
+  root to: "dashboard#show"
+end
